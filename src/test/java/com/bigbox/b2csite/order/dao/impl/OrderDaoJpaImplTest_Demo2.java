6112f26777d4df682df8e999ad9887ddc5822e9d
Ronaldo <rndtavares@gmail.com>
2017-06-25 12:22:25 -0300

    first commit

---
+++ src/test/java/com/bigbox/b2csite/order/dao/impl/OrderDaoJpaImplTest_Demo2.java
@@ -0,0 +1,67 @@
+package com.bigbox.b2csite.order.dao.impl;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.dataset.xml.XmlDataSet;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bigbox.b2csite.order.model.entity.OrderEntity;
+
+public class OrderDaoJpaImplTest_Demo2 extends BaseDBUnitTestForJPADao {
+	
+	private final static class DataFiles {
+		private final static String XML_DATA_SET = 
+				"com/bigbox/b2csite/order/dao/impl/OrderDaoJpaImplTest_XMLDataSet.xml";
+		private final static String FLAT_XML_DATA_SET = 
+				"com/bigbox/b2csite/order/dao/impl/OrderDaoJpaImplTest_FlatXMLDataSet.xml";
+		private final static String XLS_DATA_SET = 
+				"com/bigbox/b2csite/order/dao/impl/OrderDaoJpaImplTest_XlsDataSet.xls";
+	}
+	
+	private OrderDaoJpaImpl target = null;
+	
+	IDataSet dataSet = null;
+	
+	@Before
+	public void setup() throws Exception {
+		
+		target = new OrderDaoJpaImpl();
+		target.setEntityManager(entityManager);
+		
+		// Add data set initialization
+		//InputStream is = ClassLoader.getSystemResourceAsStream(DataFiles.XLS_DATA_SET);
+		//dataSet = new XlsDataSet(is);
+		
+		//InputStream is = ClassLoader.getSystemResourceAsStream(DataFiles.XML_DATA_SET);
+		//dataSet = new XmlDataSet(is);
+		
+		InputStream is =
+			ClassLoader.getSystemResourceAsStream(DataFiles.FLAT_XML_DATA_SET);
+		FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+		dataSet = builder.build(is);
+		
+		DatabaseOperation.INSERT.execute(CONN, dataSet);
+	}
+	
+	@After
+	public void teardown() throws Exception {
+		DatabaseOperation.DELETE.execute(CONN, dataSet);
+	}
+	
+	@Test
+	public void test_findByOrderSource() throws Exception {
+		
+		List<OrderEntity> resultList = target.findByOrderSource("wo");
+		
+		Assert.assertNotNull(resultList);
+		Assert.assertEquals(1, resultList.size());
+		Assert.assertEquals("ORD1", resultList.get(0).getOrderNumber());
+	}
+}
